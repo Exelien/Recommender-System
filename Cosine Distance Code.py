b2f9f812e2fb990bc5a4a372da2bb2874903d151
Exelien <68837896+Exelien@users.noreply.github.com>
2021-06-15 19:54:59 -0700

    Update Cosine Distance Code.py

--- Cosine Distance Code.py
+++ Cosine Distance Code.py
@@ -1,7 +1,7 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-text = ["London Paris London", "Paris Paris, London"]
+text = ["Jack John Jack", "John John Jack"]
 cv = CountVectorizer()
 
 countMatrix = cv.fit_transform(text)
